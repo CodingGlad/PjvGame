273f09e9520cb60b77808a9c9f8a02f34f55b1bd
Jakub Wodecki <wodecki@atlasgroup.cz>
2023-03-25 21:24:59 +0100

    added game object classes, key and types

---
+++ src/main/java/org/example/game/ObjectHandler.java
@@ -0,0 +1,18 @@
+package org.example.game;
+
+import org.example.gameobjects.GameObject;
+import org.example.gameobjects.Key;
+import org.example.gameobjects.types.KeyType;
+import org.example.gameobjects.types.ObjectType;
+import org.example.utils.WorldCoordinates;
+
+import java.util.List;
+
+import static org.example.game.GamePanel.TILE_SIZE;
+
+public class ObjectHandler {
+    public void setObjects(List<GameObject> objects) {
+        objects.add(new Key(ObjectType.KEY, KeyType.SILVER, new WorldCoordinates(23 * TILE_SIZE, 7 * TILE_SIZE)));
+        objects.add(new Key(ObjectType.KEY, KeyType.GOLD, new WorldCoordinates(23 * TILE_SIZE, 40 * TILE_SIZE)));
+    }
+}
---
+++ src/main/java/org/example/gameobjects/Key.java
@@ -0,0 +1,32 @@
+package org.example.gameobjects;
+
+import org.example.gameobjects.types.KeyType;
+import org.example.gameobjects.types.ObjectType;
+import org.example.utils.WorldCoordinates;
+
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import java.util.Objects;
+
+public class Key extends GameObject {
+    private final KeyType keyType;
+
+    public Key(ObjectType objectType, KeyType keyType, WorldCoordinates worldCoordinates) {
+        super(objectType, worldCoordinates);
+        if (objectType.equals(ObjectType.KEY)) {
+            this.keyType = keyType;
+            setKeyImage();
+        } else {
+            throw new IllegalStateException("Attempted creating object " + objectType.getName() + " as an instance of a key.");
+        }
+    }
+
+    private void setKeyImage() {
+        try {
+            setStaticImage(ImageIO.read(Objects.requireNonNull(
+                    getClass().getResourceAsStream("/sprites/objects/key/" + keyType.getName() + ".png"))));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
